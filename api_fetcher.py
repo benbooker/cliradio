e2d36f2d1349953c3551cc4f855df296d5fea290
Ben Booker <ben.o.booker@gmail.com>
2025-07-08 17:44:44 +1000

    add type hints

--- api_fetcher.py
+++ api_fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from typing import Dict, Any 
 import json 
 
 #TODO: error handling for when an endpoint is down
@@ -11,13 +12,14 @@ class StationFetcher:
 
     SERVERS = ['https://fi1.api.radio-browser.info', 'https://de1.api.radio-browser.info', 'https://de2.api.radio-browser.info']
 
-    def stations_from_search(self, query):
+    def stations_from_search(self, query: str) -> list["Station"]:
         for server in self.SERVERS:
             try:
                 url = server + '/json/stations/search?name=' + query
                 response = requests.get(url, headers={'User-Agent':'cliradio/0.0.1'})
                 data = response.json()
-                print(json.dumps(data))
+                print(type(data))
+                print(data)
                 return [Station.construct_from_json(item) for item in data] 
             except requests.RequestException:
                 continue
@@ -28,24 +30,24 @@ class StationFetcher:
 
 
 class Station:
-    def __init__(self, name, country, url, quality):
+    def __init__(self, name: str, country: str, url: str, quality: str) -> None:
         self.name = name
         self.country = country
         self.url = url
         self.quality = quality
 
-    def __str__(self):
-        name = self.name  if self.name <= 55 else self.name[:55] + "..."
+    def __str__(self) -> str:
+        name = self.name if len(self.name) <= 55 else self.name[:55] + "..."
         return name
 
 
     @staticmethod
-    def construct_from_json(data):
+    def construct_from_json(data: dict[str, Any]) -> "Station" :
         return Station(
-            data.get('name'),
-            data.get('country'),
-            data.get('url_resolved'),
-            f"{str(data.get('bitrate'))} {data.get('codec')}"
+            data.get('name', ""),
+            data.get('country', ""),
+            data.get('url_resolved', ""),
+            f"{str(data.get('bitrate', ""))} {data.get('codec', "")}"
         )
 
 
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import api_fetcher
 if __name__ == "__main__":
     fetcher = api_fetcher.StationFetcher()
     query = input('Search for radio station : ')
-    for station in fetcher.stations_from_search(query):
+    search_results = fetcher.stations_from_search(query)
+
+    for station in search_results:
         print(station)
